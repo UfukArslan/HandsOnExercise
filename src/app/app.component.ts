653b260df88f41c2d89f4cbd58e40659de8a49a2
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-06-21 23:10:11 +0200

    Initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
-import { Message } from './models/message';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'DiaLog';
-  messages: Message[];
-  
-  constructor(private api: ApiService){
-  }
-
-  ngOnInit(): void {
-    this.api.loadMessagesForDialog(2).subscribe({
-      next: (messages) => this.messages = messages,
-      error: (error) => console.log(error)
-
-    });
-  }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
-  declarations: [AppComponent, MessageComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
